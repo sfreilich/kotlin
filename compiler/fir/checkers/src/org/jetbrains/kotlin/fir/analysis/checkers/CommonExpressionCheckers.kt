ff7b04bc825b488e88cd8cdcd192d31baffb43d7
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2024-03-12 20:14:08 +0100

    fixup! [FIR] Add `FirFunctionNoInferArgumentsChecker`

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonExpressionCheckers.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonExpressionCheckers.kt
@@ -75,7 +75,7 @@ object CommonExpressionCheckers : ExpressionCheckers() {
             FirContractNotFirstStatementChecker,
             FirProtectedConstructorNotInSuperCallChecker,
             FirOptionalExpectationExpressionChecker,
-            FirFunctionNoInferArgumentsChecker
+            FirFunctionNoInferArgumentsChecker,
         )
 
     override val propertyAccessExpressionCheckers: Set<FirPropertyAccessExpressionChecker>
