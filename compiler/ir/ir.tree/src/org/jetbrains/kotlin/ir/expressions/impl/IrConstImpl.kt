a5e0da554cbd638883445806a958b7ccb6dac385
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2024-05-01 13:49:30 +0200

    [IR] Remove type parameter from `copyWithOffsets` method

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/expressions/impl/IrConstImpl.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/expressions/impl/IrConstImpl.kt
@@ -81,6 +81,6 @@ class IrConstImpl internal constructor(
     }
 }
 
-fun <T> IrConst.copyWithOffsets(startOffset: Int, endOffset: Int) =
+fun IrConst.copyWithOffsets(startOffset: Int, endOffset: Int) =
     IrConstImpl(startOffset, endOffset, type, kind, value)
 
