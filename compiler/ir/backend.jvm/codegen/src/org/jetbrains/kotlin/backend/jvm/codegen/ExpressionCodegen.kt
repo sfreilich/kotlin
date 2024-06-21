3bea9aebea98320a9e8c4ca2452d9339a873cc0e
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2024-06-21 12:11:58 +0200

    ~~~ some jvm fix ~~~

--- compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/ExpressionCodegen.kt
+++ compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/ExpressionCodegen.kt
@@ -1304,7 +1304,7 @@ class ExpressionCodegen(
         mv.nop()
         val endLabel = Label()
         val stackElement = unwindBlockStack(endLabel, data) { it.loop == jump.loop }
-        if ((jump.loop.body as? IrBlock)?.statements?.singleOrNull() is IrInlinedFunctionBlock) {
+        if ((jump.loop.body as? IrBlock)?.statements?.lastOrNull() is IrInlinedFunctionBlock) {
             // There must be another line number because this jump is actually return from inlined function
             jump.markLineNumber(startOffset = true)
         }
