f4f08ee5db87f53ebda3fe524789bb81a2716900
Nikita Nazarov <nikitanazarov@google.com>
2024-07-03 00:00:27 +0200

    Remove line numbers from parameter destructuring in lambdas

--- compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/ExpressionCodegen.kt
+++ compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/ExpressionCodegen.kt
@@ -787,11 +787,16 @@ class ExpressionCodegen(
 
         val initializer = declaration.initializer
         if (initializer != null) {
-            val value = initializer.accept(this, data)
-            initializer.markLineNumber(startOffset = true)
-            value.materializeAt(varType, declaration.type)
-            declaration.markLineNumber(startOffset = true)
-            mv.store(index, varType)
+            val shouldNotGenerateLineNumber =
+                declaration.origin == IrDeclarationOrigin.IR_DESTRUCTURED_PARAMETER_VARIABLE &&
+                        irFunction.origin != LoweredDeclarationOrigins.INLINE_LAMBDA
+            lineNumberMapper.noLineNumberScopeWithCondition(shouldNotGenerateLineNumber) {
+                val value = initializer.accept(this, data)
+                initializer.markLineNumber(startOffset = true)
+                value.materializeAt(varType, declaration.type)
+                declaration.markLineNumber(startOffset = true)
+                mv.store(index, varType)
+            }
         } else if (declaration.isVisibleInLVT) {
             declaration.markLineNumber(startOffset = true)
             pushDefaultValueOnStack(varType, mv)
