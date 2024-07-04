9491c44307b1edf993996d58ac861a1d75ab8963
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2024-07-04 19:19:31 +0200

    fixup! [stdlib] Update JS API dump

--- libraries/stdlib/api/js/kotlin.coroutines.cancellation.kt
+++ libraries/stdlib/api/js/kotlin.coroutines.cancellation.kt
@@ -1,4 +1,14 @@
 @kotlin.SinceKotlin(version = "1.4")
+@kotlin.Deprecated(level = DeprecationLevel.HIDDEN, message = "Provided for expect-actual matching")
+@kotlin.internal.InlineOnly
+public inline fun CancellationException(message: kotlin.String?, cause: kotlin.Throwable?): kotlin.coroutines.cancellation.CancellationException
+
+@kotlin.SinceKotlin(version = "1.4")
+@kotlin.Deprecated(level = DeprecationLevel.HIDDEN, message = "Provided for expect-actual matching")
+@kotlin.internal.InlineOnly
+public inline fun CancellationException(cause: kotlin.Throwable?): kotlin.coroutines.cancellation.CancellationException
+
+@kotlin.SinceKotlin(version = "1.4")
 public open class CancellationException : kotlin.IllegalStateException {
     public constructor CancellationException()
 
