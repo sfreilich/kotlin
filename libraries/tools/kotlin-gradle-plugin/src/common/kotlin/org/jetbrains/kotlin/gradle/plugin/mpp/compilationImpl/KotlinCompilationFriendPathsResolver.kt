584cd65fae6fa6257fb94601b00bfdf39073920f
Evgenii Mazhukin <evgenii.mazhukin@jetbrains.com>
2024-07-04 13:38:26 +0200

    for-science

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/compilationImpl/KotlinCompilationFriendPathsResolver.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/compilationImpl/KotlinCompilationFriendPathsResolver.kt
@@ -60,18 +60,13 @@ internal class DefaultKotlinCompilationFriendPathsResolver(
     }
 
     object DefaultFriendArtifactResolver : FriendArtifactResolver {
-        override fun resolveFriendArtifacts(compilation: InternalKotlinCompilation<*>): FileCollection {
-            return with(compilation.project) {
-                val friendArtifactsTaskProvider = resolveFriendArtifactsTask(compilation) ?: return files()
-                filesProvider { friendArtifactsTaskProvider.flatMap { it.archiveFile } }
-            }
-        }
 
-        private fun resolveFriendArtifactsTask(compilation: InternalKotlinCompilation<*>): TaskProvider<AbstractArchiveTask>? {
-            if (compilation.allAssociatedCompilations.none { it.isMain() }) return null
-            val archiveTasks = compilation.project.tasks.withType(AbstractArchiveTask::class.java)
-            if (compilation.target.artifactsTaskName !in archiveTasks.names) return null
-            return archiveTasks.named(compilation.target.artifactsTaskName)
+        override fun resolveFriendArtifacts(compilation: InternalKotlinCompilation<*>): FileCollection {
+            val files = compilation.project.files()
+            compilation.allAssociatedCompilations.forAll { associated ->
+                files.from(associated.output.classesDirs)
+            }
+            return files
         }
     }
 
