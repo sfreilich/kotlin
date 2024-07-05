99ade1c7a5dc7e2a73ddf8b01495d3dc31216d24
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-07-05 14:06:31 +0300

    [FIR] Improve the `entityTypeSuperClass.kt` test
    Make `EntityType` open to get rid `FINAL_SUPERTYPE`. This change was
    not merged into the original commit that adds the test to emphasyze why
    it's important to get rid of all `KtTypeReference`s in the next commit.
    
    ^KT-68375

--- plugins/fir-plugin-prototype/testData/diagnostics/supertypes/entityTypeSuperClass.kt
+++ plugins/fir-plugin-prototype/testData/diagnostics/supertypes/entityTypeSuperClass.kt
@@ -2,17 +2,17 @@ package foo
 
 import org.jetbrains.kotlin.fir.plugin.GeneratedEntityType
 
-class EntityType<Self>
+open class EntityType<Self>
 
-<!FINAL_SUPERTYPE!>@GeneratedEntityType
-class WithImplicitAny<!>
+@GeneratedEntityType
+class WithImplicitAny
 
 interface Inter
 
-<!FINAL_SUPERTYPE!>@GeneratedEntityType
-class WithExplicitInterface : Inter<!>
+@GeneratedEntityType
+class WithExplicitInterface : Inter
 
 open class SomeClass
 
-<!FINAL_SUPERTYPE, MANY_CLASSES_IN_SUPERTYPE_LIST!>@GeneratedEntityType
+<!MANY_CLASSES_IN_SUPERTYPE_LIST!>@GeneratedEntityType
 class WithExplicitClass : SomeClass()<!>
